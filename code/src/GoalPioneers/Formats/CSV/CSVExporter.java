f62069be819f728f6c6974bd6c18be78768dfcdb
Kent vejrup Madsen <Kent.vejrup.Madsen@outlook.com>
2022-07-04 14:05:44 +0200

    stage

--- code/src/GoalPioneers/Formats/CSV/CSVExporter.java
+++ code/src/GoalPioneers/Formats/CSV/CSVExporter.java
@@ -1,10 +1,14 @@
 package GoalPioneers.Formats.CSV;
 
 
+import GoalPioneers.Formats.Templates.FlatFiles.FlatFilesExporterFacade;
+
+
 /**
  * 
  */
 public class CSVExporter 
+        implements FlatFilesExporterFacade
 {
     // Constructor
     public CSVExporter()
--- code/src/GoalPioneers/Formats/CSV/CSVImporter.java
+++ code/src/GoalPioneers/Formats/CSV/CSVImporter.java
@@ -1,7 +1,17 @@
 package GoalPioneers.Formats.CSV;
 
+import GoalPioneers.Formats.Templates.FlatFiles.FlatFilesImporterFacade;
 
+
+/**
+ * 
+ */
 public class CSVImporter 
+        implements FlatFilesImporterFacade
 {
+    public CSVImporter()
+    {
+        
+    }
     
 }
